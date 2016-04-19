0c222863c915447e87b83103c3e4cce6ec64917a
Yan Chi Ku <duubyplz@hotmail.com>
2016-04-19 16:01:30 +1000

    autopush

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,28 +1,19 @@
-
-Object.size = function(obj) {
-    var size = 0, key;
-    for (key in obj) {
-        if (obj.hasOwnProperty(key)) size++;
-    }
-    return size;
-};
-
-// Parse & display text of selected module
+// 1. Parse & display text of selected module
 var selectedModule = $('select#contents').val();
 if (!$.isNumeric(selectedModule)) {
     $.error("Selected module isn't a valid one: " + selectedModule);
 }
-// 1. Parse
+// a) Parse
 parse(selectedModule);
 
-// 2. Display
+// b) Display
 setTimeout(function () {
     console.log("now: " + Object.keys(hash).length);
     display();
 }, 1000);
 
 
-// If module is changed
+// 2. If module is changed, reparse & display
 $('select#contents').change(function() {
     selectedModule = $('select#contents').val();
     console.log("change " + selectedModule);
@@ -36,6 +27,11 @@ $('select#contents').change(function() {
     }, 1000);
 });
 
+// 3. Check results, see if input field is correct
+
+
+
+// Subroutines
 function parse(module) {
     hash = new Object();
     var fileName = 'assets/txt/duo' + module + '.txt';
