8d489950ac1b6351ada016900df8b5691d5620af
Yan Chi Ku <duubyplz@hotmail.com>
2016-05-09 23:49:43 +1000

    change eo filter

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -91,7 +91,7 @@ function display() {
 function logic() {
     var inputString = $('textarea#answer').val();
     var sanitisedString = inputString.replace(/[^a-zA-Z0-9_\-.,?!'" ĉĝĥĵŝŭĈĜĤĴŜŬ]/g, ""); // whitelist
-    var simplifiedEO = correctEO.replace(/[.?!,:;"]/g, "").toLowerCase(); // blacklist
+    var simplifiedEO = correctEO.replace(/[^a-zA-Z0-9_\-.,?!'" ĉĝĥĵŝŭĈĜĤĴŜŬ]/g, "").toLowerCase(); // whitelist
     var simplifiedString = sanitisedString.replace(/[.?!,:"]/g, "").toLowerCase(); // blacklist
     if (simplifiedEO === simplifiedString) {
         // console.log("correct " + simplifiedString + " " + simplifiedEO);
