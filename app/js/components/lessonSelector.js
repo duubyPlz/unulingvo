29d91a12d168f138ec061d91df85f220f6121f1b
duubyPlz <duubyplz@hotmail.com>
2019-12-25 20:19:34 +1100

    comment todo

--- app/js/components/lessonSelector.js
+++ app/js/components/lessonSelector.js
@@ -1,8 +1,7 @@
 import '../jquery-global';
 import 'jquery-ui';
 
-TODO think of dependencies of this module
-https://stackoverflow.com/questions/51737267/dependency-injection-with-es6-modules
+// https://stackoverflow.com/questions/51737267/dependency-injection-with-es6-modules
 
 // XXX @kuc can't get semantic ui <select>.dropdown('set selected', random) to work
 const fileSizes = {
