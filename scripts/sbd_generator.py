72233b510de46b98959c007186d91deaac15c096
duubyPlz <duubyplz@hotmail.com>
2022-04-28 22:10:33 +1000

    middle of 4.20, sentence 2 done

--- scripts/sbd_generator.py
+++ scripts/sbd_generator.py
@@ -7,36 +7,29 @@ for Sentence Building Drill lessons.
 Output will be in the format of O: and E: (no F:).
 '''
 
+
 segment_a = {}
-segment_a["오늘부터"]="Starting from today, I am going to"
-segment_a["내일부터"]="Starting from tomorrow, I am going to"
-segment_a["지금부터"]="From now on, I am going to"
-segment_a["언제부터"]="Starting from when, am I going to"
+segment_a["아마"]="It will probably"
+segment_a["분명히"]="It will certainly"
+segment_a["어쩌면"]="It might" # manual o -지도 몰라요.
+segment_a["어쩌면2"]="It could" # manual o -ㄹ 수도 있어요.
 
 segment_b = {}
-segment_b["한국어를"]="Korean[l]"
+segment_b["내일부터 일요일까지"]="from tomorrow until Sunday."
+segment_b["내일부터 모레까지"]="from tomorrow until the day after tomorrow."
+segment_b["어제부터 오늘까지"]="from yesterday until today."
+segment_b["지난주부터 다음 주까지"]="from last week until next week."
 
 segment_c = {}
-segment_c["열심히"]="hard."
-segment_c["더 열심히"]="harder."
-segment_c["덜 열심히"]="less hard."
+segment_c["비가 내릴 거예요."]="rain[l]"
+segment_c["비가 올 거예요."]="rain"
+segment_c["눈이 내릴 거예요."]="snow[l]"
+segment_c["눈이 올 거예요."]="snow"
+segment_c["비가 그칠 거예요."]="stop raining"
+segment_c["눈이 그칠 거예요."]="stop snowing"
+segment_c["비가 많이 내릴 거예요."]="rain[l] a lot"
+segment_c["눈이 내릴 거예요."]="snow[l] a lot"
 
-segment_d = {}
-segment_d["공부할 거예요."]="study"
-segment_d["연습할 거예요."]="practice"
-segment_d["쓸 거예요."]="use"
-segment_d["말할 거예요."]="speak"
-segment_d["배울 거예요."]="learn"
-segment_d["일할 거예요."]="work on"
-segment_d["준비할 거예요."]="prepare"
-
-
-all_segments = list((
-  segment_a,
-  segment_b,
-  segment_c,
-  segment_d,
-))
 
 class AutoDict(dict):
   def __missing__(self, key):
@@ -57,10 +50,9 @@ def generate_permutations():
   for entry_a in segment_a:
     for entry_b in segment_b:
       for entry_c in segment_c:
-        for entry_d in segment_d:
-          o_sentence = f"{entry_a} {entry_b} {entry_c} {entry_d}"
-          e_sentence = f"{segment_a[entry_a]} {segment_d[entry_d]} {segment_b[entry_b]} {segment_c[entry_c]}"
-          all_sentences[o_sentence] = e_sentence
+        o_sentence = f"{entry_a} {entry_b} {entry_c}"
+        e_sentence = f"{segment_a[entry_a]} {segment_c[entry_c]} {segment_b[entry_b]}"
+        all_sentences[o_sentence] = e_sentence
 
   print_sentences(all_sentences)
         
