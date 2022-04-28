9e13bc3a253aae93dceef2b68476f34752171ced
duubyPlz <duubyplz@hotmail.com>
2022-04-28 21:47:25 +1000

    sentence building drill script, middle of 4.20

--- scripts/sbd_generator.py
+++ scripts/sbd_generator.py
@@ -7,53 +7,61 @@ for Sentence Building Drill lessons.
 Output will be in the format of O: and E: (no F:).
 '''
 
+segment_a = {}
+segment_a["오늘부터"]="Starting from today, I am going to"
+segment_a["내일부터"]="Starting from tomorrow, I am going to"
+segment_a["지금부터"]="From now on, I am going to"
+segment_a["언제부터"]="Starting from when, am I going to"
 
-segment_a = [
-  "오늘부터",
-  "내일부터",
-  "지금부터",
-  "언제부터",
-]
+segment_b = {}
+segment_b["한국어를"]="Korean[l]"
 
-segment_b = [
-  "한국어를",
-]
+segment_c = {}
+segment_c["열심히"]="hard."
+segment_c["더 열심히"]="harder."
+segment_c["덜 열심히"]="less hard."
 
-segment_c = [
-  "열심히",
-  "더 열심히",
-  "덜 열심히",
-]
+segment_d = {}
+segment_d["공부할 거예요."]="study"
+segment_d["연습할 거예요."]="practice"
+segment_d["쓸 거예요."]="use"
+segment_d["말할 거예요."]="speak"
+segment_d["배울 거예요."]="learn"
+segment_d["일할 거예요."]="work on"
+segment_d["준비할 거예요."]="prepare"
 
-segment_d = [
-  "공부할 거예요.",
-  "연습할 거예요.",
-  "쓸 거예요.",
-  "말할 거예요.",
-  "배울 거예요.",
-  "일할 거예요.",
-  "준비할 거예요.",
-  "연습할 거예요.",
-]
 
+all_segments = list((
+  segment_a,
+  segment_b,
+  segment_c,
+  segment_d,
+))
+
+class AutoDict(dict):
+  def __missing__(self, key):
+    value = self[key] = type(self)()
+    return value
 
 def print_sentences(all_sentences):
-  for sentence in all_sentences:
-    print(f"    O: {sentence}")
-    print("    E: ")
+  for key in all_sentences:
+    print(f"    O: {key}")
+    print(f"    E: {all_sentences[key]}")
     print()
 
 def generate_permutations():
   '''
   Generates all possible permutations of the given sentence phrases, based on multiple segment possibilities.
   '''
-  all_sentences = []
+  all_sentences = AutoDict()
   for entry_a in segment_a:
     for entry_b in segment_b:
       for entry_c in segment_c:
         for entry_d in segment_d:
-          sentence = f"{entry_a} {entry_b} {entry_c} {entry_d}"
-          all_sentences.append(sentence)
+          o_sentence = f"{entry_a} {entry_b} {entry_c} {entry_d}"
+          e_sentence = f"{segment_a[entry_a]} {segment_d[entry_d]} {segment_b[entry_b]} {segment_c[entry_c]}"
+          all_sentences[o_sentence] = e_sentence
+
   print_sentences(all_sentences)
         
 if __name__ == "__main__":
