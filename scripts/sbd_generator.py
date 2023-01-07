0b116eb1d681c31a1c286ce0a9e9b4e979b08f48
duubyPlz <duubyplz@hotmail.com>
2023-01-07 23:04:17 +1100

    Start ttmik 430

--- scripts/sbd_generator.py
+++ scripts/sbd_generator.py
@@ -5,36 +5,33 @@ Script to generate all permutations of sentences,
 for Sentence Building Drill lessons.
 
 Output will be in the format of O: and E: (no F:).
+
+Steps:
+  1. Edit the sections marked by `VARIABLE`
+  2. Call script with:
+    ./sbd_generator.py | pbcopy
+  3. Paste into the new assets/txt/_.txt file.
 '''
 
 # VARIABLE: Segment dictionaries
-segment_a = {}
-segment_a["내일"]="tomorrow,"
-segment_a["오늘"]="today,"
-segment_a["주말에"]="this weekend,"
-segment_a["다음 주에"]="next week,"
-segment_a["이번 달에"]="this month,"
+# segment_a = {}
+# segment_a["열 명"]="ten people[s],"
+# segment_a["한 명"]="one people[s],"
+# segment_a["두 명"]="two people[s],"
+# segment_a["세 명"]="three people[s],"
 
 segment_b = {}
-segment_b["시간이 있으면"]="If you have time"
-segment_b["시간이 없으면"]="If you don't have time"
-segment_b["시간이 많이 있으면"]="If you have a lot[av] of time"
-segment_b["시간이 많으면"]="If you have a lot[v] of time"
-segment_b["시간이 조금 밖에 없으면"]="If you have only a little bit of time"
-segment_b["시간이 전혀 없으면"]="If you have no time at all"
+# segment_b["초대했는데,"]="I have invited"
+# segment_b["말했는데,"]="I have told"
+segment_b["조심했는데,"]="I was careful"
+segment_b["열심히 공부했는데,"]="I studied hard"
 
 segment_c = {}
-segment_c["같이"]="together?"
-segment_c["저랑"]="with me?"
-segment_c["저랑 같이"]="together with me?"
-segment_c["다 같이"]="with everyone?"
-
-segment_d = {}
-segment_d["커피 마실래요?"]="do you want to drink coffee"
-segment_d["뭐 마실래요?"]="what do you want to drink"
-segment_d["어떤 거 마실래요?"]="what kind of drink do you want"
-segment_d["어디에서 마실래요?"]="where do you want to drink (something)"
-
+segment_c["아무도 안 올 수도 있어요."]="but it's possible that no one will come."
+segment_c["아무도 안 올 거예요."]="but no one will come."
+segment_c["아무도 모를 거예요."]="but no one will know."
+segment_c["아무도 안 할 거예요."]="but no one will do it."
+segment_c["아무도 초대 안 할 거예요."]="but no one will be invited."
 
 class AutoDict(dict):
   def __missing__(self, key):
@@ -55,17 +52,17 @@ def generate_permutations():
   for key_a in segment_a:
     for key_b in segment_b:
       for key_c in segment_c:
-        for key_d in segment_d:
+        # for key_d in segment_d:
           value_a = segment_a[key_a]
           value_b = segment_b[key_b]
           value_c = segment_c[key_c]
-          value_d = segment_d[key_d]
-          o_sentence = f"{key_a} {key_b} {key_c} {key_d}"
+          # value_d = segment_d[key_d]
+          o_sentence = f"{key_a} {key_b} {key_c}"
           # VARIABLE: The english sentence may need rearrangement of segments (may not be: a -> b -> c -> d)
-          e_sentence = f"{value_b} {value_a} {value_d} {value_c}"
+          e_sentence = f"{value_b} {value_a} {value_c}"
           all_sentences[o_sentence] = e_sentence
 
   print_sentences(all_sentences)
-        
+
 if __name__ == "__main__":
   generate_permutations()
