dabfdd74164e1a10f81b9da50fd90efbafa90409
duubyPlz <duubyplz@hotmail.com>
2023-01-26 04:51:30 +1100

    implement ctrl click logic

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -37,20 +37,34 @@ $('body').keydown(function(e) {
 $('.search-bar').on('click', function() {
     focusSearch();
 });
-$('.thing').on('click', function(e) {
+$('.lesson').on('click', function(e) {
     if (e.metaKey || e.ctrlKey) {
-        $(this).toggleClass('selected-thing');
+        $(this).toggleClass('selected-lesson');
     } else {
-        $('.thing').each(function() {
-            $(this).removeClass('selected-thing');
+        $('.lesson').each(function() {
+            $(this).removeClass('selected-lesson');
         });
-        $(this).addClass('selected-thing');
+        $(this).addClass('selected-lesson');
     }
 })
 
 // 2. Parse & display text of selected file(s)
-// a) Establish selected files
-var selectedModule = null;
+// a) Establish selected files/lessons
+var selectedLessons = [];
+function defineSelectedLessons() {
+    selectedLessons = []
+    $('.selected-lesson').each(function() {
+        selectedLessons.push($(this).attr('id'));
+    });
+    console.log('SELECTED LESSONS');
+    console.log(selectedLessons);
+}
+defineSelectedLessons();
+// on save, update selectedLessons
+$('#save-menu').on('click', function() {
+    defineSelectedLessons();
+    hideMenu();
+});
 
 // b) Parse
 // parse(selectedModule, language);
