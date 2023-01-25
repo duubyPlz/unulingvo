73a62adb57fc72f9ea46018b8a3b1da75a7e352c
duubyPlz <duubyplz@hotmail.com>
2023-01-26 02:52:44 +1100

    start rich menu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,37 @@
-console.log("app started.")
+// 0. Init
+// Global hash representation of current file
+var fileHash = new Object();
+// Current file's top badge (short-form to full name) hash
+var badgeHash = new Object();
+
+// 1. Parse & display text of selected file(s)
+// Listeners
+function showMenu() {
+    $('.rich-menu').show();
+    $('#app').hide();
+}
+function hideMenu() {
+    $('.rich-menu').hide();
+    $('#app').show();
+}
+$('.top-panel').on('click', function() {
+    showMenu();
+});
+$('button.close-menu').on('click', function() {
+    hideMenu();
+});
+$('body').keydown(function(e) {
+    if ($('.rich-menu').is(':visible')) {
+        if (e.keyCode == 27 || e.keyCode == 13)  { // esc or enter
+            e.preventDefault();
+            hideMenu();
+        }
+    }
+});
+
+// a) Establish selected files
+var selectedModule = null;
+
+// b) Parse
+// parse(selectedModule, language);
+
