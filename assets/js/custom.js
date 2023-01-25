a9b985a68cc6719adcc46b2b80de3161f77e8d91
duubyPlz <duubyplz@hotmail.com>
2023-01-26 03:42:00 +1100

    rich menu search bar

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,8 +4,7 @@ var fileHash = new Object();
 // Current file's top badge (short-form to full name) hash
 var badgeHash = new Object();
 
-// 1. Parse & display text of selected file(s)
-// Listeners
+// 1. Rich menu listeners
 function showMenu() {
     $('.rich-menu').show();
     $('#app').hide();
@@ -17,7 +16,7 @@ function hideMenu() {
 $('.top-panel').on('click', function() {
     showMenu();
 });
-$('button.close-menu').on('click', function() {
+$('button#close-menu').on('click', function() {
     hideMenu();
 });
 $('body').keydown(function(e) {
@@ -28,7 +27,11 @@ $('body').keydown(function(e) {
         }
     }
 });
+$('.search-bar').on('click', function() {
+    $('.search-bar').val('');
+});
 
+// 2. Parse & display text of selected file(s)
 // a) Establish selected files
 var selectedModule = null;
 
