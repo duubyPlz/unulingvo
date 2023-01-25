2d28f2bb5df003e548c374d882bc91448ce54ad9
duubyPlz <duubyplz@hotmail.com>
2023-01-26 04:55:59 +1100

    implement ctrl click logic

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,5 @@
-// 0. Init
-// Global hash representation of current file
-var fileHash = new Object();
-// Current file's top badge (short-form to full name) hash
-var badgeHash = new Object();
 
-// 1. Rich menu listeners
+// Functions
 function focusSearch() {
     $('.search-bar').val('');
     $('.search-bar').focus();
@@ -20,6 +15,31 @@ function hideMenu() {
     $('#app').show();
     $('.logo').show();
 }
+function saveMenu() {
+    defineSelectedLessons();
+    hideMenu();
+}
+function defineSelectedLessons() {
+    selectedLessons = []
+    $('.selected-lesson').each(function() {
+        selectedLessons.push($(this).attr('id'));
+    });
+    console.log('SELECTED LESSONS');
+    console.log(selectedLessons);
+}
+function parse(selectedLessons, language) {
+    console.log('PARSE');
+    console.log(selectedLessons);
+    console.log(language);
+}
+
+// 0. Init
+// Global hash representation of current file
+var fileHash = new Object();
+// Current file's top badge (short-form to full name) hash
+var badgeHash = new Object();
+
+// 1. Rich menu listeners
 $('.top-panel').on('click', function() {
     showMenu();
 });
@@ -28,9 +48,12 @@ $('button#close-menu').on('click', function() {
 });
 $('body').keydown(function(e) {
     if ($('.rich-menu').is(':visible')) {
-        if (e.keyCode == 27 || e.keyCode == 13)  { // esc or enter
+        if (e.keyCode == 27)  { // esc
             e.preventDefault();
             hideMenu();
+        } else if (e.keyCode == 13) { // enter
+            e.preventDefault();
+            saveMenu();
         }
     }
 });
@@ -51,21 +74,12 @@ $('.lesson').on('click', function(e) {
 // 2. Parse & display text of selected file(s)
 // a) Establish selected files/lessons
 var selectedLessons = [];
-function defineSelectedLessons() {
-    selectedLessons = []
-    $('.selected-lesson').each(function() {
-        selectedLessons.push($(this).attr('id'));
-    });
-    console.log('SELECTED LESSONS');
-    console.log(selectedLessons);
-}
 defineSelectedLessons();
-// on save, update selectedLessons
+// On menu save, update selectedLessons
 $('#save-menu').on('click', function() {
-    defineSelectedLessons();
-    hideMenu();
+    saveMenu();
 });
 
 // b) Parse
-// parse(selectedModule, language);
+parse(selectedLessons, 'KO');
 
